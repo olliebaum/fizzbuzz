ff489923ee375b53f7758e077ef60980c27d9e17
olliebaum <oliver@baums.org.uk>
2018-08-23 15:07:19 +0100

    Added test expecting 4 if given 4. Changed code to pass.

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -1,4 +1,5 @@
 def fizzbuzz(number)
   return 'fizz' if number == 3
   return 'buzz' if number == 5
+  return number
 end
--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -6,4 +6,7 @@ describe 'fizzbuzz' do
   it 'returns "buzz" when passed 5' do
     expect(fizzbuzz(5)).to eq 'buzz'
   end
+  it 'returns 4 when passed 4' do
+    expect(fizzbuzz(4)).to eq 4
+  end
 end
